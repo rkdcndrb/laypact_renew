6a1ceb5eed095e558013d9a3d051c244b83e8c81
강승미 <thme808@thme808.local>
2023-08-24 00:38:24 +0900

    first commit

---
+++ src/main/java/com/laypact/LaypactApplication.java
@@ -0,0 +1,13 @@
+package com.laypact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaypactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaypactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laypact/laypact/LaypactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laypact.laypact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaypactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
